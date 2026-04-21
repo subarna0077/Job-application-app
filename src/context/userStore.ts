1ac2c2b9a65f6b915e8affaf0de8c570344553d8
subarna0077 <sumansapkota0099@gmail.com>
2026-04-21 19:03:05 +0545

    minor fix

--- src/context/userStore.ts
+++ src/context/userStore.ts
@@ -67,7 +67,7 @@ export const useAuthStore = create<AuthStore>((set) => ({
     },
 
     register: async (formData) => {
-        const response = await fetch('http://localhost:3001/register', {
+        const response = await fetch(`${API_BASE_URL}/register`, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(formData)
