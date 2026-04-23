8b7fca2b2800979fe905eb24b2971bd0a58b93d3
subarna0077 <sumansapkota0099@gmail.com>
2026-04-23 18:17:51 +0545

    Tanstack query configuration

--- src/main.tsx
+++ src/main.tsx
@@ -5,18 +5,41 @@ import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 import { ApplicationContextProvider } from './context/ApplicationContext.tsx'
 import { ThemeContextProvider } from './context/ThemeContext.tsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { UserContextProvider } from './context/UserContext.tsx'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+      retry: 2
+    }
+  }
+})
 
 createRoot(document.getElementById('root')!).render(
-<ThemeContextProvider>
-  <BrowserRouter>
+  <QueryClientProvider client={queryClient}>
+    <ThemeContextProvider>
+     
+      <BrowserRouter>
+       <UserContextProvider>
+         <ApplicationContextProvider>
+            <StrictMode>
+              <App />
+            </StrictMode>
+          </ApplicationContextProvider>
+
+       </UserContextProvider>
+        
+
+         
+       
+
+      </BrowserRouter>
    
-      <ApplicationContextProvider>
-        <StrictMode>
-          <App />
-        </StrictMode>
-      </ApplicationContextProvider>
-  </BrowserRouter>
-  </ThemeContextProvider>
+
+    </ThemeContextProvider>
+  </QueryClientProvider>
 
   ,
 )
